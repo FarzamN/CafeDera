0004d8ab51001722a0faf89a8e64a552a2bee5e1
Farzam <frzamn64ml@gmail.com>
2025-07-23 20:04:27 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="flex w-full h-screen">
-        {/* <Sidebar /> */}
+        <Sidebar />
         <div className="flex-1 overflow-y-auto bg-gray-50 p-6">
           <Routes>
             <Route path="/" element={<Dashboard />} />
